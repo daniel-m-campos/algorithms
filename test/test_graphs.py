c22b9673ef4f6e0bf9b7cfae8b25483c20b5f71d
Daniel M Campos <daniel.m.campos@icloud.com>
2020-02-02 10:37:29 -0600

    Start tests

---
+++ test/test_graphs.py
@@ -0,0 +1,22 @@
+from unittest import TestCase
+
+import graphs
+
+
+class TestGraphs(TestCase):
+    def test_morning_topolical_sort(self):
+        plan = {
+            7: [6, 8],
+            3: [],
+            4: [2, 5],
+            1: [2],
+            2: [],
+            5: [2, 6],
+            6: [9],
+            8: [9],
+            9: [],
+        }
+        result = graphs.depth_first_search2(plan)
+        actual = result["topological_sort"]
+        expected = [1, 4, 5, 2, 3, 7, 6, 8, 9]
+        self.assertListEqual(actual, expected)
