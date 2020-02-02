83cc60114d7e5102a96fab4cfdf8a83db4a583d4
Daniel M Campos <daniel.m.campos@icloud.com>
2020-02-02 14:47:41 -0600

    Finish big graph scc

--- test/test_big_graph.py
+++ test/test_big_graph.py
@@ -1,27 +1,36 @@
+import threading
 from unittest import TestCase
 
 import graphs
+
+threading.stack_size(67108864)
 
 BIG_GRAPH_SIZE = 875714
 
 
 def get_big_graph(file="../../SCC.txt"):
-    big_graph = {}
-    for i in range(1, BIG_GRAPH_SIZE + 1):
-        big_graph[i] = []
+    big_graph = {i: [] for i in range(1, BIG_GRAPH_SIZE + 1)}
+    t_big_graph = {i: [] for i in range(1, BIG_GRAPH_SIZE + 1)}
     with open(file) as file:
         for line in file.readlines():
-            edge = line.split(" ")
+            edge = line.split()
             big_graph[int(edge[0])].append(int(edge[1]))
-    return big_graph
+            t_big_graph[int(edge[1])].append(int(edge[0]))
+    return big_graph, t_big_graph
 
 
 class TestBigGraph(TestCase):
-    big_graph = get_big_graph()
+    big_graph, t_big_graph = get_big_graph()
 
     def test_size(self):
         self.assertEqual(len(self.big_graph), BIG_GRAPH_SIZE)
 
     def test_big_scc(self):
-        scc = graphs.find_scc(self.big_graph)
-        print(scc.most_common(5))
+        def main():
+            scc = graphs.find_scc(self.big_graph, self.t_big_graph)
+            top_5 = scc.most_common(5)
+            print(top_5)
+            print(",".join(str(t[1]) for t in top_5))
+
+        thread = threading.Thread(target=main)
+        thread.start()
