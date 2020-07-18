21e2203e80475588abb6f8e6c78dfe1cd6133a2d
Daniel M Campos <daniel.m.campos@icloud.com>
2020-07-18 17:34:14 -0500

    Solve week 3 assignment 3 Q1-2
    - Answers are Q1:19, Q2:9

--- test/integration/test_huffman.py
+++ test/integration/test_huffman.py
@@ -1,14 +1,17 @@
-from unittest import TestCase
+import pytest
 
 import huffman
 from test.integration import util
 
 
-class TestHuffman(TestCase):
-    def test_huffman_10_40_input(self):
-        array = util.get_array("../../../huffman_10_40_input.txt")
-        weights = {str(k): w for k, w in enumerate(array[1:])}
-        code = huffman.code(weights)
-        lengths = huffman.lengths(code)
-        assert min(lengths) == 4
-        assert max(lengths) == 9
+@pytest.mark.parametrize(
+    "file, min_length, max_length",
+    [("../../../huffman_10_40_input.txt", 4, 9), ("../../../huffman.txt", 9, 19)],
+)
+def test_code_lengths(file, min_length, max_length):
+    array = util.get_array(file)
+    weights = {str(k): w for k, w in enumerate(array[1:])}
+    code = huffman.code(weights)
+    lengths = huffman.lengths(code)
+    assert min(lengths) == min_length
+    assert max(lengths) == max_length
