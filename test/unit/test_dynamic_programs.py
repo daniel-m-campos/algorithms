e3ad786fe1fe76abefa27cbf89af5e359f5dc944
Daniel M Campos <daniel.m.campos@icloud.com>
2020-07-18 19:01:18 -0500

    Fix test

--- test/unit/test_dynamic_programs.py
+++ test/unit/test_dynamic_programs.py
@@ -4,4 +4,4 @@ import dynamic_programs
 def test_four_vertex_example():
     max_weight, vertices = dynamic_programs.mwis(weights=[1, 4, 5, 4])
     assert max_weight == 8
-    assert vertices == [1, 3]
+    assert vertices == [2, 4]
